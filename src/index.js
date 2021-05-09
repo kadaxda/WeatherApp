f9e7b06f65a2678b8d5c00715b5cb1579c9aca74
kadaxda <Dr.J4kob@gmail.com>
2021-05-09 10:03:20 +0200

    sometimes 0.000001 degree

--- src/index.js
+++ src/index.js
@@ -5,23 +5,86 @@ async function getWeatherData(city) {
   );
   const weatherData = await response.json();
   return {
-    temperature: weatherData.main.temp,
-    feelsLike: weatherData.main.feels_like,
+    temperature: kToC(weatherData.main.temp),
     weatherDes: weatherData.weather[0].main,
     obj: weatherData,
   };
 }
-
-let weather = getWeatherData("London");
+// Init
+let weather = getWeatherData("Berlin");
 weather.then((res) => {
   console.log(res);
+  let info = document.querySelector(".info");
+
+  let city = document.createElement("h3");
+  city.textContent = "Berlin";
+  let temp = document.createElement("h3");
+  temp.setAttribute("id", "temp");
+  temp.textContent = res.temperature;
+  let des = document.createElement("h3");
+  des.textContent = res.weatherDes;
+
+  info.append(city, temp, des);
+  changeBackground(res.weatherDes);
 });
 
 const search = document.querySelector("#search");
 const searchBtn = document.querySelector("#searchBtn");
+const swapBtn = document.querySelector("#swapBtn");
+
 searchBtn.addEventListener("click", () => {
   let weather = getWeatherData(`${search.value}`);
   weather.then((res) => {
     console.log(res);
+    let info = document.querySelector(".info");
+    info.innerHTML = "";
+
+    let city = document.createElement("h3");
+    city.textContent = search.value;
+    let temp = document.createElement("h3");
+    temp.setAttribute("id", "temp");
+    temp.textContent = res.temperature;
+    let des = document.createElement("h3");
+    des.textContent = res.weatherDes;
+
+    info.append(city, temp, des);
+    changeBackground(res.weatherDes);
   });
 });
+
+swapBtn.addEventListener("click", () => {
+  let temp = document.querySelector("#temp");
+  let curTemp = temp.textContent;
+  if (swapBtn.textContent == "°F") {
+    temp.textContent = cToF(curTemp);
+    swapBtn.textContent = "°C";
+    break;
+  } else if (swapBtn.textContent == "°C") {
+    temp.textContent = fToC(curTemp);
+    swapBtn.textContent = "°F";
+  }
+});
+
+function changeBackground(wetter) {
+  console.log(wetter);
+  if (wetter == "Rain") {
+    document.body.style.backgroundImage = "url('css/rain.jpg')";
+  } else if (wetter == "Clouds") {
+    document.body.style.backgroundImage = "url('css/clouds.jpg')";
+  } else if (wetter == "Clear") {
+    document.body.style.backgroundImage = "url('css/sun.jpg')";
+  }
+}
+
+function cToF(temp) {
+  let a = temp * (9 / 5) + 32;
+  return Math.round(a * 100) / 100;
+}
+function fToC(temp) {
+  let a = (temp - 32) * (5 / 9);
+  return Math.round(a * 100) / 100;
+}
+function kToC(temp) {
+  let a = temp - 273.15;
+  return Math.round(a * 100) / 100;
+}
